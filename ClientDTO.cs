49e34b3ad60cfb92da6c73d303fc1ffed57b75ca
ccmmasi2 <144745598+ccmmasi2@users.noreply.github.com>
2023-09-30 12:29:21 -0500

    Add files via upload

---
+++ ClientDTO.cs
@@ -0,0 +1,41 @@
+﻿using Invoicing.AccessData.DTOs.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace Invoicing.AccessData.DTOs
+{
+    public class ClientDTO : BaseDTO
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        public string Identificacion { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(50, ErrorMessage = "The length of the field should be less than 50")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(50, ErrorMessage = "The length of the field should be less than 50")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        public DateTime BirthDay { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(50, ErrorMessage = "The length of the field should be less than 50")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(50, ErrorMessage = "The length of the field should be less than 50")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(50, ErrorMessage = "The length of the field should be less than 100")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(100, ErrorMessage = "The length of the field should be less than 100")]
+        public string Email { get; set; }
+    }
+}
